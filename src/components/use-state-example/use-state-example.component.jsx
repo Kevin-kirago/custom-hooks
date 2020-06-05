305dc8a88aac9a49150574848c9afcb7e8f79807
Kevin <tkevinkiragu@gmail.com>
2020-06-05 23:57:29 +0300

    learning hooks

--- src/components/use-state-example/use-state-example.component.jsx
+++ src/components/use-state-example/use-state-example.component.jsx
@@ -1,44 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Card from '../card/card.component';
+import Card from "../card/card.component";
 
 const UseStateExample = () => {
-  const [name, setName] = useState('Yihua');
-  const [address, setAddress] = useState('Amsterdam');
+	const [name, setName] = useState("Yihua");
+	const [address, setAddress] = useState("Amsterdam");
 
-  return (
-    <Card>
-      <h1> {name} </h1>
-      <h1> {address} </h1>
-      <button onClick={() => setName('Andrei')}>Set Name to Andrei</button>
-      <button onClick={() => setAddress('Canada')}>Set Address</button>
-    </Card>
-  );
+	return (
+		<Card>
+			<h1> {name} </h1>
+			<h1> {address} </h1>
+			<button onClick={() => setName("Andrei")}>Set Name to Andrei</button>
+			<button onClick={() => setAddress("Canada")}>Set Address</button>
+		</Card>
+	);
 };
 
 export class StateClassComponent extends React.Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      name: 'Yihua',
-      address: 'Canada'
-    };
-  }
+		this.state = {
+			name: "Yihua",
+			address: "Canada",
+		};
+	}
 
-  render() {
-    return (
-      <Card>
-        <h1> {this.state.name} </h1>
-        <button onClick={this.setState({ name: 'Andrei' })}>
-          Set Name to Andrei
-        </button>
-        <button onClick={this.setState({ address: 'Amsterdam' })}>
-          Set Address
-        </button>
-      </Card>
-    );
-  }
+	render() {
+		return (
+			<Card>
+				<h1> {this.state.name} </h1>
+				<button onClick={this.setState({ name: "Andrei" })}>Set Name to Andrei</button>
+				<button onClick={this.setState({ address: "Amsterdam" })}>Set Address</button>
+			</Card>
+		);
+	}
 }
 
 export default UseStateExample;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
-import './index.css';
-
-import App from './App';
-
-import { store } from './redux/store';
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
 );
